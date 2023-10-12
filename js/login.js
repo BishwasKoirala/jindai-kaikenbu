4519819985049c02a11b3eb1f50cad62a495e294
Bishwas Koirala <wasubisu69@gmail.com>
2023-10-12 23:51:48 +0900

    committing

---
+++ js/login.js
@@ -0,0 +1,23 @@
+function LoginUi() {
+  clickLoginButton = document.querySelector(".login-page-button").addEventListener("click", () => {
+    document.querySelector(".the-page").innerHTML = `<h1>LOG-IN</h1>
+    <form action="">
+      <label for="user-id">学籍番号</label>
+      <br />
+      <input type="text" name="user-id" id="user-id" />
+      <br>
+      <label for="password">パスワード</label>
+      <br>
+      <input type="text" name="password" id="password" />
+      <br><br>
+      <button id="login">
+        Log-in
+      </button>
+      <br>
+      <a href="kasidasi.html">仮ログイン</a>
+
+    </form>`;
+  });
+}
+
+document.querySelector('.login-page-button').addEventListener("click",LoginUi())
