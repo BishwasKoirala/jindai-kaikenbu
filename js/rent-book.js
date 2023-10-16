213107047d66f2992993806f305b0d1f52f2adec
BishwasKoirala <wasubisu69@gmail.com>
2023-10-16 13:02:46 +0900

    did some code changes

--- js/rent-book.js
+++ js/rent-book.js
@@ -1,25 +1,27 @@
-const rentBookHtml = `
+let bookHtml = {
+  rentBook:`
   <h3>本の拝借登録</h3>
-    <select name="rent-book" id="rent-book">
-      <option value="null" selected>本を選択</option>
-      <option value="exmpbook">簿記一級参考書</option>
-    </select>
-    <button>登録</button>
-`
-const returnBookHtml = `
+  <select name="rent-book" id="rent-book">
+    <option value="null" selected>本を選択</option>
+    <option value="exmpbook">簿記一級参考書</option>]]
+  </select>
+  <button>登録</button>
+  `,
+  returnBook:`
   <h3>本の返却登録</h3>
-    <select name="return-book" id="return-book">
-      <option value="null" selected>本を選択</option>
-      <option value="exmpbook">簿記一級参考書</option>
-    </select>
-    <button>登録</button>
-`
+  <select name="return-book" id="return-book">
+    <option value="null" selected>本を選択</option>
+    <option value="exmpbook">簿記一級参考書</option>
+  </select>
+  <button>登録</button>
+  `
+}
 
 let generateHtml = document.querySelector('.generate')
 document.querySelector('.rent-book-button').addEventListener('click',() => {
-  generateHtml.innerHTML= rentBookHtml
+  generateHtml.innerHTML= bookHtml.rentBook
 })
 
 document.querySelector('.return-book-button').addEventListener('click',() => {
-  generateHtml.innerHTML= returnBookHtml
+  generateHtml.innerHTML= bookHtml.returnBook
 })
